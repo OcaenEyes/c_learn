0fec3dfb48007dc7867ffbbf19b9cefc02114900
OcaenEyes <1450136519@qq.com>
2020-09-09 20:46:12 +0800

    master

--- c++/base_knowledge/variabletypes.cpp
+++ c++/base_knowledge/variabletypes.cpp
@@ -6,11 +6,30 @@ extern int a,b;
 extern int c;
 extern float f;
 
+// 全局变量
+int e;
+// 全局变量未赋值时，则会自动初始化
+// 数据类型	初始化默认值
+// int	0
+// char	'\0'
+// float	0
+// double	0
+// pointer	NULL
+
+//函数声明
+int func();
+
 int main(){
     //变量定义
     int a,b;
     int c;
     float f;
+
+
+    // 局部变量
+    int d;
+
+    //在函数内，局部变量的值会覆盖全局变量的值
 
     // 变量初始化
     a=10;
@@ -21,5 +40,15 @@ int main(){
     f = 70.0 /3.0;
     cout << f << endl;
 
+    // 函数调用
+    int i = func();
+
     return 0;
 }
+
+
+//函数定义
+int func(){
+    return 0;
+}
+
