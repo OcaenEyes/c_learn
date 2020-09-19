c7202b514c597bdc5f65f204bb2e5411845de11f
OcaenEyes <1450136519@qq.com>
2020-09-19 18:21:38 +0800

    master

--- c++/base_knowledge/基本的输入输出.cpp
+++ c++/base_knowledge/基本的输入输出.cpp
@@ -11,8 +11,57 @@
 <fstream>	该文件为用户控制的文件处理声明服务。我们将在文件和流的相关章节讨论它的细节。
 */
 
-
 // 标准输出流 cout
 // 标准输入流 cin
 // 标准错误流 cerr
 // 标准日志流 clog
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int shuchu();
+int shuru();
+int cuowu();
+int rizhi();
+
+int main()
+{
+    int i = shuchu();
+    int j = shuru();
+    int k = cuowu();
+    int l = rizhi();
+    return 0;
+}
+
+int shuchu()
+{
+    string i = "hello c++";
+    cout << "输出语句"
+         << "\n";
+    cout << "i value is" << i << "\n";
+    return 0;
+}
+
+int shuru(){
+    string i;
+    cout << "请输入你的姓名" << "\n";
+    cin >> i;
+    cout << "您输入的姓名是：" << i << "\n";
+
+    return 0;
+}
+
+int cuowu(){
+    string i = "出现错误了";
+
+    cerr << "Error message:" <<i << "\n";
+    return 0;
+}
+
+int rizhi(){
+    string i = "这是一个日志";
+
+    cerr << "Log message:" <<i << "\n";
+    return 0;
+}
