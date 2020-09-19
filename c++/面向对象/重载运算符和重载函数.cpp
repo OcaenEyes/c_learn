2d5a460d7c5af511a4bd9e5cfa0b2c4f8930fae3
OcaenEyes <1450136519@qq.com>
2020-09-20 00:22:30 +0800

    master

--- c++/面向对象/重载运算符和重载函数.cpp
+++ c++/面向对象/重载运算符和重载函数.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+// 重载函数
 class printData
 {
 public:
@@ -20,6 +21,45 @@ public:
     }
 };
 
+
+// 重载运算符
+class Box
+{
+   public:
+ 
+      double getVolume(void)
+      {
+         return length * breadth * height;
+      }
+      void setLength( double len )
+      {
+          length = len;
+      }
+ 
+      void setBreadth( double bre )
+      {
+          breadth = bre;
+      }
+ 
+      void setHeight( double hei )
+      {
+          height = hei;
+      }
+      // 重载 + 运算符，用于把两个 Box 对象相加
+      Box operator+(const Box& b)
+      {
+         Box box;
+         box.length = this->length + b.length;
+         box.breadth = this->breadth + b.breadth;
+         box.height = this->height + b.height;
+         return box;
+      }
+   private:
+      double length;      // 长度
+      double breadth;     // 宽度
+      double height;      // 高度
+};
+
 int main()
 {
     printData pd;
@@ -33,3 +73,27 @@ int main()
     pd.print(c);
     return 0;
 }
+
+
+// 可重载运算符
+/*
+双目算术运算符	+ (加)，-(减)，*(乘)，/(除)，% (取模)
+关系运算符	==(等于)，!= (不等于)，< (小于)，> (大于>，<=(小于等于)，>=(大于等于)
+逻辑运算符	||(逻辑或)，&&(逻辑与)，!(逻辑非)
+单目运算符	+ (正)，-(负)，*(指针)，&(取地址)
+自增自减运算符	++(自增)，--(自减)
+位运算符	| (按位或)，& (按位与)，~(按位取反)，^(按位异或),，<< (左移)，>>(右移)
+赋值运算符	=, +=, -=, *=, /= , % = , &=, |=, ^=, <<=, >>=
+空间申请与释放	new, delete, new[ ] , delete[]
+其他运算符	()(函数调用)，->(成员访问)，,(逗号)，[](下标)
+*/
+
+// 不可重载运算符
+/*
+        .：成员访问运算符
+        .*, ->*：成员指针访问运算符
+        ::：域运算符
+        sizeof：长度运算符
+条件     ?:：条件运算符
+        #： 预处理符号
+*/
