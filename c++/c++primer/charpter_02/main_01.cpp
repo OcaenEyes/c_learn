ee1613c45d9bba63f3e3c0ac530e026d2bbf826e
OcaenEyes <1450136519@qq.com>
2022-05-21 09:17:31 +0800

    master

--- c++/c++primer/charpter_02/main_01.cpp
+++ c++/c++primer/charpter_02/main_01.cpp
@@ -3,7 +3,11 @@ using namespace std;
 
 int main()
 {
-    cout << "hello"
+    cout << "hello111"
          << "\n";
+    cout << "hello222"
+         << "\n";
+    int i = 1;
+    cout << i;
     return 0;
 }
