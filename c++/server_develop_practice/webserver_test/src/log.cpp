5547ddb5c18fee3dc0abe8d6215fdd08560f797b
OcaenEyes <1450136519@qq.com>
2022-08-05 20:19:30 +0800

    Update log.cpp

--- c++/server_develop_practice/webserver_test/src/log.cpp
+++ c++/server_develop_practice/webserver_test/src/log.cpp
@@ -2,7 +2,7 @@
  * @Author: OCEAN.GZY
  * @Date: 2022-08-05 08:29:28
  * @LastEditors: OCEAN.GZY
- * @LastEditTime: 2022-08-05 12:04:34
+ * @LastEditTime: 2022-08-05 12:17:22
  * @FilePath: /c++/server_develop_practice/webserver_test/src/log.cpp
  * @Description: 注释信息
  */
@@ -56,33 +56,55 @@ bool log::init(const char *file_name, int close_log, int log_buf_size, int split
     else
     {
         strcpy(log_name, p + 1);
-        strncpy(dir_name,file_name,p-file_name +1);
-        snprintf(log_full_name,255,"%s%d_%02d_%02d_%s",dir_name,my_tm.tm_year + 1900, my_tm.tm_mon + 1, my_tm.tm_mday, file_name);
+        strncpy(dir_name, file_name, p - file_name + 1);
+        snprintf(log_full_name, 255, "%s%d_%02d_%02d_%s", dir_name, my_tm.tm_year + 1900, my_tm.tm_mon + 1, my_tm.tm_mday, file_name);
     }
 
     m_today = my_tm.tm_mday;
 
-    m_fp = fopen(log_full_name,"a");
+    m_fp = fopen(log_full_name, "a");
     if (m_fp == NULL)
     {
         return false;
     }
     return true;
-    
 }
 
-
-
-void log::write_log(int level,const char *format,...){
-    struct timeval now ={0,0};
-    gettimeofday(&now,NULL);
+void log::write_log(int level, const char *format, ...)
+{
+    struct timeval now = {0, 0};
+    gettimeofday(&now, NULL);
     time_t t = now.tv_sec;
     struct tm *sys_tem = localtime(&t);
-    
+    struct tm my_tm = *sys_tem;
+    char s[16] = {0};
+    switch (level)
+    {
+    case 0:
+        strcpy(s, "[debug]:");
+        break;
+    case 1:
+        strcpy(s, "[info]:");
+        break;
+    case 2:
+        strcpy(s, "[warn]:");
+        break;
+    case 3:
+        strcpy(s, "[error]:");
+        break;
+    default:
+        strcpy(s, "[info]:");
+        break;
+    }
 
+    // 写入一个log，对m_count ++ , m_split_lines最大行数
+    m_mutex.lock();
+    m_count ++;
+    
 }
 
-void log::flush(void){
+void log::flush(void)
+{
     m_mutex.lock();
     //强制刷新写入流缓冲区
     fflush(m_fp);
