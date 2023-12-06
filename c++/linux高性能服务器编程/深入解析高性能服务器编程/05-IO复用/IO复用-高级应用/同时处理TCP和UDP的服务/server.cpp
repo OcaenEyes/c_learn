2b82733bdd38604c7901c0f595c0b8b36b586c91
OcaenEyes <1450136519@qq.com>
2023-12-06 22:38:53 +0800

    update

--- c++/linux高性能服务器编程/深入解析高性能服务器编程/05-IO复用/IO复用-高级应用/同时处理TCP和UDP的服务/server.cpp
+++ c++/linux高性能服务器编程/深入解析高性能服务器编程/05-IO复用/IO复用-高级应用/同时处理TCP和UDP的服务/server.cpp
@@ -2,7 +2,7 @@
  * @Author: OCEAN.GZY
  * @Date: 2023-12-0608:48:46
  * @LastEditors: OCEAN.GZY
- * @LastEditTime: 2023-12-06 12:07:48
+ * @LastEditTime: 2023-12-06 22:28:59
  * @FilePath: /c++/linux高性能服务器编程/深入解析高性能服务器编程/05-IO复用/IO复用-高级应用/同时处理TCP和UDP的服务/server.cpp
  * @Description: 同时处理TCP和UDP的回射服务器
  *
@@ -82,7 +82,94 @@ int main(int argc, char *argv[])
     // 创建TCP socket,并将其绑当到端口port上
     int listenfd = socket(PF_INET, SOCK_STREAM, 0);
     assert(listenfd >= 0);
-
     ret = bind(listenfd, (struct sockaddr *)&address, sizeof(address));
     assert(ret != -1);
+    ret = listen(listenfd, 5);
+    assert(ret != -1);
+
+    // 创建UDP socket,并将其绑定到端口port上
+    bzero(&address, sizeof(address));
+    address.sin_family = AF_INET;
+    inet_pton(AF_INET, ip, &address.sin_addr);
+    address.sin_port = htons(port);
+    int udpfd = socket(PF_INET, SOCK_DGRAM, 0);
+    assert(udpfd >= 0);
+    ret = bind(udpfd, (struct sockaddr *)&address, sizeof(address));
+    assert(ret != -1);
+
+    epoll_event events[MAX_EVENT_NUMBER];
+    int epollfd = epoll_create(5);
+    assert(epollfd != -1);
+
+    // 注册TCPsocket 和UDPsocket 上的可读事件
+    addfd(epollfd, listenfd);
+    addfd(epollfd, udpfd);
+
+    while (1)
+    {
+        int number = epoll_wait(epollfd, events, MAX_EVENT_NUMBER, -1);
+        if (number < 0)
+        {
+            printf("epoll失败\n");
+            break;
+        }
+        for (int i = 0; i < number; i++)
+        {
+            int sockfd = events[i].data.fd;
+            if (sockfd == listenfd)
+            {
+                struct sockaddr_in client_addr;
+                socklen_t client_addr_length = sizeof(client_addr);
+                int connfd = accept(listenfd, (struct sockaddr *)&client_addr, &client_addr_length);
+                addfd(epollfd, connfd);
+            }
+            else if (sockfd == udpfd)
+            {
+                char buf[UDP_BUFFER_SIZE];
+                memset(buf, '\0', UDP_BUFFER_SIZE);
+                struct sockaddr_in client_addr;
+                socklen_t client_addr_length = sizeof(client_addr);
+
+                ret = recvfrom(udpfd, buf, UDP_BUFFER_SIZE - 1, 0,
+                               (struct sockaddr *)&client_addr, &client_addr_length);
+
+                if (ret > 0)
+                {
+                    sendto(udpfd, buf, UDP_BUFFER_SIZE - 1, 0,
+                           (struct sockaddr *)&client_addr, client_addr_length);
+                }
+            }
+            else if (events[i].events & EPOLLIN)
+            {
+                char buf[TCP_BUFFER_SZIE];
+                while (1)
+                {
+                    memset(buf, '\0', TCP_BUFFER_SZIE);
+                    ret = recv(sockfd, buf, TCP_BUFFER_SZIE - 1, 0);
+                    if (ret < 0)
+                    {
+                        if ((errno == EAGAIN) || (errno == EWOULDBLOCK))
+                        {
+                            break;
+                        }
+                        close(sockfd);
+                        break;
+                    }
+                    else if (ret == 0)
+                    {
+                        close(sockfd);
+                    }
+                    else
+                    {
+                        send(sockfd, buf, ret, 0);
+                    }
+                }
+            }
+            else{
+                printf("something else hapened\n");
+            }
+        }
+    }
+    close(listenfd);
+    return 0;
 }
---
+++ c++/linux高性能服务器编程/深入解析高性能服务器编程/05-IO复用/超级服务xinetd.md
@@ -0,0 +1,9 @@
+<!--
+ * @Author: OCEAN.GZY
+ * @Date: 2023-12-06 22:34:50
+ * @LastEditors: OCEAN.GZY
+ * @LastEditTime: 2023-12-06 22:35:51
+ * @FilePath: /c++/linux高性能服务器编程/深入解析高性能服务器编程/05-IO复用/超级服务xinetd.md
+ * @Description: 注释信息
+-->
+# 超级服务xinetd
