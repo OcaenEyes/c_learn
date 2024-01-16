7c93aca22346f8e913bf411dc241313cae3dd949
OcaenEyes <1450136519@qq.com>
2024-01-16 21:39:51 +0800

    Update ocean_mprpc_controller.h

--- c++/knowledge/c++mprpc分布式网络通信框架/src/include/ocean_mprpc_controller.h
+++ c++/knowledge/c++mprpc分布式网络通信框架/src/include/ocean_mprpc_controller.h
@@ -1,10 +1,32 @@
+/*
+ * @Author: OCEAN.GZY
+ * @Date: 2024-01-16 21:33:05
+ * @LastEditors: OCEAN.GZY
+ * @LastEditTime: 2024-01-16 21:38:11
+ * @FilePath: /c++/knowledge/c++mprpc分布式网络通信框架/src/include/ocean_mprpc_controller.h
+ * @Description: 注释信息
+ */
 #pragma once
+
+#include <string>
+#include <google/protobuf/service.h>
 
 class OCEANMprpcController
 {
 private:
-    /* data */
+    bool m_failed;  // rpc方法执行过程中的状态
+    std::string m_errtext;  // rpc方法执行过程中的错误信息
 public:
     OCEANMprpcController(/* args */);
     ~OCEANMprpcController();
+
+    void Reset();
+    bool Failed() const;
+    std::string ErrorText() const;
+    void SetFailed(const std::string& reson);
+    
+
+    void StartCancel();
+    bool IsCanceled() const;
+    void NotifyOnCancel(google::protobuf::Closure* callback);
 };
