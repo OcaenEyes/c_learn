c12c8f67f6e99fdaeee52e84b10afec9cbef1746
OcaenEyes <1450136519@qq.com>
2024-01-16 21:15:36 +0800

    update

---
+++ c++/knowledge/c++mprpc分布式网络通信框架/src/include/ocean_mprpc_controller.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class OCEANMprpcController
+{
+private:
+    /* data */
+public:
+    OCEANMprpcController(/* args */);
+    ~OCEANMprpcController();
+};
---
+++ c++/knowledge/c++mprpc分布式网络通信框架/src/ocean_mprpc_controller.cc
@@ -0,0 +1,11 @@
+/*
+ * @Author: OCEAN.GZY
+ * @Date: 2024-01-16 11:44:05
+ * @LastEditors: OCEAN.GZY
+ * @LastEditTime: 2024-01-16 11:50:51
+ * @FilePath: /c++/knowledge/c++mprpc分布式网络通信框架/src/ocean_mprpc_controller.cc
+ * @Description: 注释信息
+ */
+
+
+#include "ocean_mprpc_controller.h"
